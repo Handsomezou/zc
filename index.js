80b73f93c83396ad9d584fea60090a3e655308b1
zoucheng <825475394@qq.com>
2023-02-01 14:45:48 +0800

    移除了index.js和new.css

--- index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-  var div = document.querySelector('div')
-  console.log(div);
-  div.addEventListener('click', () => {
-    div.style.color = 'red'
-  })
-})
